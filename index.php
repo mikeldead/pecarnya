c5d24c0d7961a28e3b04f764de2b84dfeb73d5a8
astina-asta <astina-asta@yandex.ru>
2019-04-30 20:45:37 +0400

    Разбил вывод от модели

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
 <?php
-echo "hello"
+
+
+$i = 1
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<?php if ($i ==1)
+	echo "hello"; ?>
+</body>
+</html>
